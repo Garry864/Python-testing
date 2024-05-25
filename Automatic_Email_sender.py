69144d671f66a1dcf3063f3cf9dc2d2509363040
Garry864 <gouravy86451@gmail.com>
2024-05-26 00:23:13 +0530

    Envionment variables removed

--- Automatic_Email_sender.py
+++ Automatic_Email_sender.py
@@ -4,19 +4,24 @@ from email.message import EmailMessage
 from email.utils import formataddr
 from pathlib import Path
 
-from dotenv import load_dotenv # pip install python-dotenv
+# from dotenv import load_dotenv # pip install python-dotenv
 
 port = 587
 EMAIL_SERVER = "smtp.gmail.com"
 
 # Load the enviorment variables
-current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
-envars = current_dir/ ".env"
-load_dotenv(envars)
+# current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
+# envars = current_dir/ ".env"
+# load_dotenv(envars)
 
 # Read the enviorment variables
-sender_email = os.getenv("EMAIL")
-password_email = os.getenv("PASSWORD")
+# sender_email = os.getenv("EMAIL")
+# password_email = os.getenv("PASSWORD")
+
+# Authentic_id
+
+sender_email = "aaravsingh86451@gmail.com"
+password_email = "qfqo qrlp azvw btnt"
 
 def send_email(subject, reciever_mail, name, due_date, invoice_no, amount):
     # Create the base text message
